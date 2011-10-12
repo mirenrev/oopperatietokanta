cbdbbdd41fda2384df27ab3546964d308f7505d6
Jussi Miestamo <vernerim@gmail.com>
2011-10-12 23:51:30 +0300

    Illan päätteeksi, vähän ehkä jotain

--- lisaa.tpl
+++ lisaa.tpl
@@ -1,7 +1,17 @@
 <p>Lisaa tietokantaan asioita</p>
 <form action="/lisaa" method="GET">
-	<input type = "text" size="50" maxlength="40" name="oopnimi" value = "Oopperan nimi">
-	<input type = "text" size = "50" maxlength ="40" name = "saveltaja" value = "Oopperan säveltäjä">
-	<input type = "text" size = "50" maxlength = "40" name = "esityspaiva" value = "esim. 2011-10-12">
-<input type="submit" name = "save" value"save">
+	<input type = "text" size="50" maxlength="40" name="oopnimi" value = "Oopperan nimi"/>
+	<input type = "text" size = "50" maxlength ="40" name = "saveltaja" value = "Oopperan säveltäjä"/>
+	<input type = "text" size = "50" maxlength = "40" name = "esityspaiva" value = "esim. 2011-10-12"/>	
+	<input type = "text" size = "50" maxlength = "40" name = "esityspaiva" value = "esim. 2011-10-12"/>	
+	<input type = "text" size = "50" maxlength = "40" name = "rooli1_nimi" value = "Roolin 1 nimi"/>
+	<input type = "checkbox" name = "onko1_esiintyja" checked = "checked"/>
+	<input type = "text" size = "50" maxlength = "40" name = "rooli1_aaniala" value = "Roolin 1 ääniala"/>
+	<input type = "text" size = "50" maxlength = "40" name = "rooli2_nimi" value = "Roolin 2 nimi"/>	
+	<input type = "checkbox" name = "onko2_esiintyja" checked = "checked"/>
+	<input type = "text" size = "50" maxlength = "40" name = "rooli2_aaniala" value = "Roolin 2 ääniala"/>
+	<input type = "text" size = "50" maxlength = "40" name = "rooli3_nimi" value = "Roolin 3 nimi"/>
+	<input type = "checkbox" name = "onko3_esiintyja" checked = "checked"/>
+	<input type = "text" size = "50" maxlength = "40" name = "rooli3_aaniala" value = "Roolin 3 ääniala"/>	
+	<input type="submit" name = "save" value"save">
 </form>
--- oopperaprojekti.py
+++ oopperaprojekti.py
@@ -16,6 +16,11 @@ def lisaa_ooppera(yhteys,oopnimi,saveltaja):
 	avain = yhteys.query("select currval('oop_id')").getresult()[0][0]
 	return avain
 
+def lisaa_oopperaan_rooli(yhteys,ooppera_id,roolinimi,aaniala,onko_esiintyja):
+	lisays = "insert into rooli values (DEFAULT,'" + ooppera_id + "','" + roolinimi + "','" + aaniala + "','" + onko_esiintyja + "')"
+	yhteys.query(lisays)
+	avain = yhteys.query("select currval('rool_id')")
+	return avain
 
 # Alustava funktio kaiken mahdollisen tiedon lisaamiseen tietokantaan
 
@@ -26,9 +31,16 @@ def lisaa_kantaan():
 		print ooppera
 		saveltaja = request.GET.get('saveltaja','').strip()
 		paiva = request.GET.get('esityspaiva','').strip()
+		rooli1_nimi = request.GET.get('rooli1_nimi','').strip()
+		onko1_esiintyja = request.GET.get('onko1_esiintyja','').strip()
+		if onko1_esiintyja == 'on':
+			onko1_esiintyja = 'true'
+		else:
+			onko1_esiintyja = 'false'
+		rooli1_aaniala = request.GET.get('rooli1_aaniala','').strip()
         	yhteys = yhdista('oopperatietokanta','localhost','verneri','kissa')
-		oop_avain = lisaa_ooppera(yhteys,ooppera,saveltaja)
-		print oop_avain
+		oop_avain = str(lisaa_ooppera(yhteys,ooppera,saveltaja))
+		rool_avain = str(lisaa_oopperaan_rooli(yhteys,oop_avain,rooli1_nimi,rooli1_aaniala,onko1_esiintyja))
 		yhteys.close()
 		return "<p>Onnistui! %s</p>" % (oop_avain)
 	else:
@@ -39,7 +51,7 @@ def lisaa_kantaan():
 @oop.route('/tulossivu')
 def tulossivu():
 	yhteys = yhdista('oopperatietokanta','localhost','verneri','kissa')
-	tulos = yhteys.query("select ooppera_id,oopnimi,saveltaja from ooppera")
+	tulos = yhteys.query("select ooppera.ooppera_id,rooli.ooppera_id,oopnimi,saveltaja,roolinimi,aaniala,esiintyja from ooppera inner join rooli ON (ooppera.ooppera_id = rooli.ooppera_id)")
 	pal = tulos.getresult()
 	output = template('ooptaulukko',rivit=pal)
 	yhteys.close()
